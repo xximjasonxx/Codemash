fc4d6c5a342df88a131765385e895131a4ebcb81
xximjasonxx <imjason@gmail.com>
2012-09-22 09:22:49 -0500

    Completed tests for Speaker Controller in the DeltaApi project

--- Codemash/Codemash.DeltaApi/Controllers/SpeakerController.cs
+++ Codemash/Codemash.DeltaApi/Controllers/SpeakerController.cs
@@ -1,13 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Net;
-using System.Net.Http;
 using System.Web.Http;
+using Codemash.Api.Data.Entities;
+using Codemash.Api.Data.Repositories;
+using Ninject;
 
 namespace Codemash.DeltaApi.Controllers
 {
     public class SpeakerController : ApiController
     {
+        [Inject]
+        public ISpeakerRepository SpeakerRepository { get; set; }
+
+        public IEnumerable<Speaker> Get()
+        {
+            return SpeakerRepository.GetAll();
+        }
+
+        public Speaker Get(int id)
+        {
+            var result = SpeakerRepository.Get(id);
+            if (result == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            return result;
+        }
     }
 }

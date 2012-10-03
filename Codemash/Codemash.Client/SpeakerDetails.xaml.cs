f4ee67c805c224385f875beb6909a8f8e2b4eeca
xximjasonxx <imjason@gmail.com>
2012-10-02 20:17:55 -0500

    Flow is created

--- Codemash/Codemash.Client/SpeakerDetails.xaml.cs
+++ Codemash/Codemash.Client/SpeakerDetails.xaml.cs
@@ -1,9 +1,8 @@
-﻿using Codemash.Client.Data;
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using Codemash.Client.Data;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -14,16 +13,17 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Grouped Items Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234231
+// The Item Detail Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234232
 
 namespace Codemash.Client
 {
     /// <summary>
-    /// A page that displays a grouped collection of items.
+    /// A page that displays details for a single item within a group while allowing gestures to
+    /// flip through other items belonging to the same group.
     /// </summary>
-    public sealed partial class Main : Codemash.Client.Common.LayoutAwarePage
+    public sealed partial class SpeakerDetails : Codemash.Client.Common.LayoutAwarePage
     {
-        public Main()
+        public SpeakerDetails()
         {
             this.InitializeComponent();
         }
@@ -39,21 +39,11 @@ namespace Codemash.Client
         /// session.  This will be null the first time a page is visited.</param>
         protected override void LoadState(Object navigationParameter, Dictionary<String, Object> pageState)
         {
-            // TODO: Create an appropriate data model for your problem domain to replace the sample data
-            //var sampleDataGroups = SampleDataSource.GetGroups((String)navigationParameter);
-            var sampleDataGroups = SessionDataSource.GetGroups();
-            this.DefaultViewModel["Groups"] = sampleDataGroups;
-        }
-
-        /// <summary>
-        /// Invoked when an item within a group is clicked.
-        /// </summary>
-        /// <param name="sender">The GridView (or ListView when the application is snapped)
-        /// displaying the item clicked.</param>
-        /// <param name="e">Event data that describes the item clicked.</param>
-        void ItemView_ItemClick(object sender, ItemClickEventArgs e)
-        {
-            this.Frame.Navigate(typeof(SessionDetails), e.ClickedItem);
+            var speaker = (Speaker) navigationParameter;
+            tbSpeakerName.Text = speaker.SpeakerName;
+            tbBiography.Text = speaker.Biography;
+            tbTwitter.Text = "@" + speaker.Twitter;
+            tbEmail.Text = "Email Me";
         }
     }
 }

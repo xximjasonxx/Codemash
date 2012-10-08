38a8f674c8bdf50b2e5250b00ce290323fbb3bef
xximjasonxx <imjason@gmail.com>
2012-10-07 21:29:54 -0500

    Began building data access infrastructure

---
+++ Codemash/Clients/Codemash.Client/Container/ClientContainer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ninject;
+
+namespace Codemash.Client.Container
+{
+    public class ClientContainer : StandardKernel
+    {
+        public ClientContainer()
+        {
+            
+        }
+    }
+}
---
+++ Codemash/Codemash.Client.Data/Modules/RepositoryModule.cs
@@ -0,0 +1,17 @@
+﻿using Codemash.Client.Data.Repository;
+using Codemash.Client.Data.Repository.Impl;
+
+namespace Codemash.Client.Data.Modules
+{
+    public class RepositoryModule : Ninject.Modules.NinjectModule
+    {
+        #region Overrides of NinjectModule
+
+        public override void Load()
+        {
+            Bind<ISessionRepository>().To<JsonSessionRepository>().InSingletonScope().Named("Session");
+        }
+
+        #endregion
+    }
+}

000e0a39d87b9c9d956d76a0a4c4ff365190e8c9
xximjasonxx <imjason@gmail.com>
2012-10-14 21:31:46 -0500

    Worked on making consistent style for List Items in the GridView

---
+++ Codemash/Clients/Codemash.Client/Core/SessionItemContainerStyleSelector.cs
@@ -0,0 +1,23 @@
+﻿using Codemash.Client.Code;
+using Codemash.Client.DataModels;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Codemash.Client.Core
+{
+    public class SessionItemContainerStyleSelector : StyleSelector
+    {
+        protected override Style SelectStyleCore(object item, DependencyObject container)
+        {
+            var listItem = item as IListItem;
+            if (listItem != null)
+            {
+                if (listItem.ItemType == ItemType.GroupHeading)
+                    return App.Current.Resources["GroupHeaderItem"] as Style;
+                return App.Current.Resources["GroupItem"] as Style;
+            }
+
+            return null;
+        }
+    }
+}
